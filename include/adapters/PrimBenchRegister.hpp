8f750d09dfbd9c6bda2a0194effa1ffd0055c15b
Côme Eyraud <come.eyraud@polymtl.ca>
2026-04-28 17:41:27 -0400

    fixing primbench macros

--- include/adapters/PrimBenchRegister.hpp
+++ include/adapters/PrimBenchRegister.hpp
@@ -8,24 +8,24 @@
 /// Internal implementation macros
 /// ---------------------------------------------------------------------------
 
-#define PRIMBENCH_REGISTER_WORKLOAD_IMPL(WorkloadType, UniqueSuffix)                                                   \
+#define PRIMBENCH_REGISTER_WORKLOAD_IMPL(WorkloadType)                                                                 \
   namespace {                                                                                                          \
-    static const Adapters::RegisterWorkload<WorkloadType> _primbench_reg_##UniqueSuffix{};                             \
+    static const Adapters::RegisterWorkload<WorkloadType> _primbench_reg_##WorkloadType{};                             \
   }
 
-#define PRIMBENCH_REGISTER_KERNEL_IMPL(KernelType, UniqueSuffix)                                                       \
+#define PRIMBENCH_REGISTER_KERNEL_IMPL(KernelType)                                                                     \
   namespace {                                                                                                          \
-    static const Adapters::RegisterWorkload<Baseliner::KernelWorkload<KernelType>> _primbench_reg_##UniqueSuffix{};    \
+    static const Adapters::RegisterWorkload<Baseliner::KernelWorkload<KernelType>> _primbench_reg_##KernelType{};      \
   }
 
 /// ---------------------------------------------------------------------------
 /// Public macros
 /// ---------------------------------------------------------------------------
 
-#define PRIMBENCH_REGISTER_WORKLOAD(Workload) PRIMBENCH_REGISTER_WORKLOAD_IMPL(Workload, __COUNTER__)
+#define PRIMBENCH_REGISTER_WORKLOAD(Workload) PRIMBENCH_REGISTER_WORKLOAD_IMPL(Workload)
 
-#define PRIMBENCH_REGISTER_WORKLOAD_NAME(Workload, name) PRIMBENCH_REGISTER_WORKLOAD_IMPL(Workload, __COUNTER__)
+#define PRIMBENCH_REGISTER_WORKLOAD_NAME(Workload, name) PRIMBENCH_REGISTER_WORKLOAD_IMPL(Workload)
 
-#define PRIMBENCH_REGISTER_KERNEL(Kernel) PRIMBENCH_REGISTER_KERNEL_IMPL(Kernel, __COUNTER__)
+#define PRIMBENCH_REGISTER_KERNEL(Kernel) PRIMBENCH_REGISTER_KERNEL_IMPL(Kernel)
 
 #endif // ADAPTER_PRIMBENCHREGISTER_HPP
