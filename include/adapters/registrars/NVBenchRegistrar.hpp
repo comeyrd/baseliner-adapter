68b21c096f2939ba41dfe6c7bf9950abce3c1608
Côme Eyraud <come.eyraud@polymtl.ca>
2026-05-07 21:54:27 -0400

    Updating to have the flop throughput and memory for nvbench

--- include/adapters/registrars/NVBenchRegistrar.hpp
+++ include/adapters/registrars/NVBenchRegistrar.hpp
@@ -19,7 +19,14 @@ namespace Adapters {
       workload.setup_host();
 
       workload.setup_device(state.get_cuda_stream());
-
+      auto flop = workload.number_of_floating_point_operations();
+      if (flop.has_value()) {
+        state.add_element_count(flop.value());
+      }
+      auto bytes = workload.number_of_bytes();
+      if (bytes.has_value()) {
+        state.add_global_memory_reads<float>(bytes.value());
+      }
       state.exec(nvbench::exec_tag::sync, [&](nvbench::launch &launch) {
         workload.reset_device(launch.get_stream());
         workload.run(launch.get_stream());
